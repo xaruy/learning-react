fe9a8d6a83b6c09d283f16a3fd8f3a11504ad8d0
XioaRui <xiaorui@leither.org>
2018-05-10 14:42:10 +0800

    react-redux练习

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 
 module.exports = {
-    entry: './src/app.jsx',
+    entry: './src/index.js',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
